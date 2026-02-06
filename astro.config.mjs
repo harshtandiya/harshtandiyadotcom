0ca9231b756a27288d5f2c1124b2984a62c848b4
Harsh Tandiya <harsh.tandiya@gmail.com>
2026-02-06 20:26:43 +0530

    feat: integrate React support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -11,4 +13,5 @@ export default defineConfig({
   },
 
   adapter: cloudflare(),
+  integrations: [react()],
 });
